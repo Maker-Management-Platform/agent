9e415d367fe880935dd090893d9083f959fa707f
Nuno Gama Freire <nunardas@gmail.com>
2024-03-02 17:43:59 +0000

    Remove apikey from printers response

--- core/integrations/printers/endpoints.go
+++ core/integrations/printers/endpoints.go
@@ -15,6 +15,7 @@ import (
 func index(c echo.Context) error {
 	rtn := make([]*models.Printer, 0)
 	for _, p := range state.Printers {
+		p.ApiKey = ""
 		rtn = append(rtn, p)
 	}
 	return c.JSON(http.StatusOK, rtn)
@@ -23,7 +24,7 @@ func index(c echo.Context) error {
 func show(c echo.Context) error {
 	uuid := c.Param("uuid")
 	printer, ok := state.Printers[uuid]
-
+	printer.ApiKey = ""
 	if !ok {
 		return c.NoContent(http.StatusNotFound)
 	}
