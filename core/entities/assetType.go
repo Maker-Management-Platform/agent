56673b37fbaa9e9611d350a037d35474307eaf26
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2024-03-08 17:33:55 +0000

    Asset type order for the UI #110

--- core/entities/assetType.go
+++ core/entities/assetType.go
@@ -4,4 +4,5 @@ type AssetType struct {
 	Name       string   `json:"name" mapstructure:"name"`
 	Label      string   `json:"label" mapstructure:"label"`
 	Extensions []string `json:"extensions" mapstructure:"extensions"`
+	Order      int      `json:"order" mapstructure:"order"`
 }
