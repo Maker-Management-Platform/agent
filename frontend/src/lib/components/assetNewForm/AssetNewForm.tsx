8fa5e028fca5cc3d3c7f7e8416d9f3bfc28ab720
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2025-02-21 21:07:09 +0000

    sync asset create form

--- frontend/src/lib/components/assetNewForm/AssetNewForm.tsx
+++ frontend/src/lib/components/assetNewForm/AssetNewForm.tsx
@@ -1,4 +1,15 @@
-import { Asset } from "@/lib/models";
+import { newFolderPost, patchAsset } from "@/lib/fetchers/getAsset";
+import { Asset, AssetPatch, NewFolderPost } from "@/lib/models";
+import {
+  Button,
+  Fieldset,
+  Grid,
+  Group,
+  Textarea,
+  TextInput,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function AssetNewForm({
   parent,
@@ -9,7 +20,91 @@ export function AssetNewForm({
 }) {
   return (
     <>
-      <div>a potato</div>
+      <Grid grow>
+        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+          <Fieldset legend="Upload"></Fieldset>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+          <Fieldset legend="New Folder">
+            <NewFolder
+              parent={parent}
+              callback={() => {
+                onClose();
+              }}
+            />
+          </Fieldset>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+          <Fieldset legend="Import">
+            <ImportList
+              parent={parent}
+              callback={() => {
+                onClose();
+              }}
+            />
+          </Fieldset>
+        </Grid.Col>
+      </Grid>
     </>
+  );
+}
+
+interface nestedProps {
+  parent: Asset;
+  callback: () => void;
+}
+
+function NewFolder({ parent, callback }: nestedProps) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: newFolderPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["asset", parent.ID] });
+      callback();
+    },
+  });
+
+  const form = useForm<NewFolderPost>({
+    initialValues: {
+      ParentID: parent.ID,
+      FolderName: "",
+    } as NewFolderPost,
+    validate: {
+      FolderName: (value) =>
+        !value || value.length > 0 ? undefined : "Folder name is required",
+    },
+  });
+
+  const formSubmit = (values: AssetPatch): Promise<any> => {
+    return mutation.mutateAsync(values);
+  };
+
+  return (
+    <form onSubmit={form.onSubmit(formSubmit)} onReset={form.reset}>
+      <TextInput
+        placeholder="Folder name"
+        key={form.key("FolderName")}
+        {...form.getInputProps("FolderName")}
+      />
+      <Group align="flex-end" mt="lg">
+        <Button type="submit">Create</Button>
+      </Group>
+    </form>
+  );
+}
+
+function ImportList({ parent, callback }: nestedProps) {
+  return (
+    <form>
+      <Textarea
+        placeholder="List of links"
+        minRows={5}
+        maxRows={10}
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Group align="flex-end" mt="lg">
+        <Button onClick={callback}>Import</Button>
+      </Group>
+    </form>
   );
 }
--- frontend/src/lib/fetchers/getAsset.ts
+++ frontend/src/lib/fetchers/getAsset.ts
@@ -1,39 +1,59 @@
-import { useQuery } from "@tanstack/react-query"
-import { Asset, AssetPatch } from "../models"
-
+import { useQuery } from "@tanstack/react-query";
+import { Asset, AssetPatch, NewFolderPost } from "../models";
 
 export async function getAsset(assetId?: string): Promise<Asset> {
-    const response = await fetch(`http://localhost:8000/api/lib${assetId ? '/' + assetId : ''}`)
-    if (!response.ok) {
-        throw new Error('Network response was not ok')
-    }
-    return response.json()
+  const response = await fetch(
+    `http://localhost:8000/api/lib${assetId ? "/" + assetId : ""}`,
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
 }
 
 export function useGetAsset(assetId?: string) {
-    return useQuery({
-        queryKey: ['asset', assetId],
-        queryFn: async (): Promise<Asset> => {
-            const response = await fetch(`http://localhost:8000/api/lib${assetId ? '/' + assetId : ''}`)
-            if (!response.ok) {
-                throw new Error('Network response was not ok')
-            }
-            return response.json()
-        }
-    })
+  return useQuery({
+    queryKey: ["asset", assetId],
+    queryFn: async (): Promise<Asset> => {
+      const response = await fetch(
+        `http://localhost:8000/api/lib${assetId ? "/" + assetId : ""}`,
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
 }
-
 
 export async function patchAsset(asset: AssetPatch): Promise<Asset> {
-    const response = await fetch(`http://localhost:8000/api/lib/${asset.ID}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(asset)
-    })
-    if (!response.ok) {
-        throw new Error('Network response was not ok')
-    }
-    return response.json()
+  const response = await fetch(`http://localhost:8000/api/lib/${asset.ID}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(asset),
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
 }
+
+export async function newFolderPost(input: NewFolderPost): Promise<Asset> {
+  const response = await fetch(
+    `http://localhost:8000/api/lib/${input.ParentID}/folder`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    },
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+}
+
