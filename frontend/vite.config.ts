a0ffd33c45125445edaae2fbed0f55b9ebac1f12
Tom Hiller <thrilleratplay@gmail.com>
2025-11-20 09:36:12 -0500

    chore(vite.config): add checker and ts path plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [
-            { find: '@', replacement: '/src' },
-            { find: '@tabler/icons-react', replacement: '@tabler/icons-react/dist/esm/icons/index.mjs' }
-        ],
-    },
-})
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      { find: '@tabler/icons-react', replacement: '@tabler/icons-react/dist/esm/icons/index.mjs' },
+    ],
+  },
+});
